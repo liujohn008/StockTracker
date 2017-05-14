64ee59ed4a9ba444072739057658fc7b9fd93f9f
liujohn008 <liujohn008@gmail.com>
2017-05-13 23:46:15 -0400

    first version

--- StockTracker/ViewModel/StockTrackerViewModel.cs
+++ StockTracker/ViewModel/StockTrackerViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,6 +44,21 @@ namespace StockTracker.ViewModel
             }
         }
 
+        private List<HistoricalStock> _stockPrice = new List<HistoricalStock>();
+        public List<HistoricalStock> StockPrice
+        {
+            get
+            {
+                return _stockPrice;
+            }
+            set
+            {
+                _stockPrice = value;
+                NotifyPropertyChanged();
+            }
+
+        }
+
         public ICommand Run { get; private set; }
 
 
@@ -52,10 +68,48 @@ namespace StockTracker.ViewModel
 
         public void OnRun(object arg)
         {
-            MessageBox.Show(string.Format("Success Running.. Ticker Name: {0}", SelectedTicker));
+            //MessageBox.Show(string.Format("Success Running.. Ticker Name: {0}", SelectedTicker));
+            //this.StockPrice = DownloadData(this.SelectedTicker, 2015);
+
+            var t = Task.Run(() => DownloadData(SelectedTicker, 2015));
+            
         }
 
-    #endregion function
+        public void DownloadData(string ticker, int yearToStartFrom)
+        {
+            List<HistoricalStock> retval = new List<HistoricalStock>();
+            using (WebClient web = new WebClient())
+            {
+                string data = web.DownloadString(string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&c={1}", ticker, yearToStartFrom));
+
+                data = data.Replace("r", "");
+
+                string[] rows = data.Split('\n');
+
+                //First row is headers so Ignore it
+                for (int i = 1; i < rows.Length; i++)
+                {
+                    if (rows[i].Replace("n", "").Trim() == "") continue;
+
+                    string[] cols = rows[i].Split(',');
+
+                    HistoricalStock hs = new HistoricalStock();
+                    hs.Date = Convert.ToDateTime(cols[0]);
+                    hs.Open = Convert.ToDouble(cols[1]);
+                    hs.High = Convert.ToDouble(cols[2]);
+                    hs.Low = Convert.ToDouble(cols[3]);
+                    hs.Close = Convert.ToDouble(cols[4]);
+                    hs.Volume = Convert.ToDouble(cols[5]);
+                    hs.AdjClose = Convert.ToDouble(cols[6]);
+
+                    retval.Add(hs);
+                }
+
+                this.StockPrice = retval;
+            }
+        }
+
+        #endregion function
 
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -74,5 +128,16 @@ namespace StockTracker.ViewModel
         GOOGL,
         AAPL,
         BABA
+    }
+
+    public class HistoricalStock
+    {
+        public DateTime Date { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public double Volume { get; set; }
+        public double AdjClose { get; set; }
     }
 }
